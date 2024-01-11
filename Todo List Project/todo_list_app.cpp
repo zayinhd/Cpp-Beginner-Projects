c60a045167a77365e3d17388388308e672388f0b
zayin <zayinm300@gmail.com>
2024-01-11 20:23:54 +0000

    Add Functions to TodoList

--- Todo List Project/todo_list_app.cpp
+++ Todo List Project/todo_list_app.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "sqlite3.h"
 
 using namespace std;
 
@@ -10,31 +11,19 @@ struct Task
     bool completed;
 };
 
-void showTodoList(const vector<Task> &todoList)
-{
-}
-void addTodoItem(vector<Task> &todoList)
-{
-}
-void completeTodoItem(vector<Task> &todoList)
-{
-}
-void deleteTodoItem(vector<Task> &todoList)
-{
-}
-void clearTodoList(vector<Task> &todoList)
-{
-}
-int main()
-{
-    vector<Task> todoList;
+// Function declarations
+void start(int choice, vector<Task> &todoList);
+void showTodoList(const vector<Task> &todoList);
+void addTodoItem(vector<Task> &todoList);
+void completeTodoItem(vector<Task> &todoList);
+void deleteTodoItem(vector<Task> &todoList);
+void clearTodoList(vector<Task> &todoList);
 
-    int choice;
-
-    do
+void start(int choice, vector<Task> &todoList){
+	do
     {
 
-        cout << "\n\t\tToDo List Management\n";
+        cout << "\n\t\tToDo List Management App\n";
         cout << "\t     ---------------------------\n";
         cout << "1. Show Todo List\n";
         cout << "2. Add Todo Item\n";
@@ -70,6 +59,101 @@ int main()
             break;
         }
     } while (choice != 6);
+}
+
+void showTodoList(const vector<Task> &todoList)
+{
+	cout<<"\n\t\tTodo List\n";
+	cout << "------------------------\n";
+    cout << "Status\tDescription\n";
+    cout << "------------------------\n";
+    
+    for(const auto& task : todoList){
+    	cout<<(task.completed ? "Done\t" : "Pending\t")<<task.description<<
+	}
+	cout << "------------------------\n";
+}
+void addTodoItem(vector<Task> &todoList)
+{
+	Task newTask;
+	cout<<"What are you planning to do?\n";
+	cin.ignore();
+	getline(cin, newTask.description);
+	
+	newTask.completed = false;
+	
+	todoList.push_back(newTask);
+	
+	cout<<"New Task added to Todo List.\n";
+	
+}
+void completeTodoItem(vector<Task> &todoList)
+{
+	int index;
+	cout<<"Enter the index of the task to mark as Completed : ";
+	cin>>index;
+	
+	if(index >= 0 && index < todoList.size()){
+		todoList[index].completed = true;
+		cout<<"Task marked as completed.\n";
+	}
+	else{
+		cout << "Invalid index.\n";
+	}
+}
+void deleteTodoItem(vector<Task> &todoList)
+{
+	int index;
+	cout<<"Enter the index of the task to Delete : ";
+	cin>>index;
+	
+	if(index >= 0 && index < todoList.size()){
+		todoList.erase(index);
+		cout<<"Task removed from Todo List.\n";
+	}
+	else{
+		cout<<"Task not found in Todo List.\n";
+	}
+}
+void clearTodoList(vector<Task> &todoList)
+{
+	for(int i = 0; i < todoList.size(); i++){
+		todoList.erase(i);
+	}
+	cout<<"Todo List Cleared.\n";
+}
+int main()
+{
+    vector<Task> todoList;
+
+    int choice;
+	
+	//Create User credentials DB
+	sqlite3* db;
+	
+	int rc = sqlite3_open("user_credentials.db", &db);
+	if (rc != SQLITE_OK){
+		cerr<"Cannot open the database: "<<sqlite3_errmsg(db)<<endl;
+		return rc;
+	}
+	
+	else{
+	const char* insertDataQuery = "INSERT INTO UserCredentials (Username, Password) VALUES (?,?);";
+	
+	sqlite3_stmt* stmt;
+	rc = sqlite3_prepare_v2(db, insertDataQuery, -1, &stmt, 0);
+	
+	sqlite3_bind_text(stmt, 1, "use1", -1, SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 2, "password1", -1, SQLITE_STATIC);
+	
+	rc = sqlite3_step(stmt);
+	
+	sqlite3_finalize(stmt);
+	const char* createTableQuery = "CREATE TABLE IF NOT EXISTS UserCredentials (ID INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT NOT NULL, Password TEXT NOT NULL;";
+	
+	}
+	
+    start(choice, todoList);
 
     return 0;
 }
